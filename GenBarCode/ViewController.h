4f4e2d35c94d88d527f7ae2d70bbbcb62f663786
Steve Yeom <krazyeom@gmail.com>
2014-09-18 22:40:24 +0900

    Initial Commit

---
+++ GenBarCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GenBarCode
+//
+//  Created by Steve Yeom on 9/18/14.
+//  Copyright (c) 2014 2nd Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
